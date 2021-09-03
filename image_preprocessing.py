225acf3e02032d37b3e368fd529bfcb97a1f9fef
Gokul <75981297+g0kul6@users.noreply.github.com>
2021-09-03 13:37:37 +0530

    image transformations and creating dataloader for train,val and test

---
+++ image_preprocessing.py
@@ -0,0 +1,66 @@
+import numpy as np
+from torchvision import transforms
+from torch.utils.data import DataLoader,Dataset
+from PIL import Image
+from hyperparameter import batch_size
+
+#tranformations to train images
+train_transforms=transforms.Compose([transforms.Resize((224,224)),
+                                    transforms.RandomHorizontalFlip(),
+                                    transforms.ToTensor()])
+#transformation to val images
+val_transforms=transforms.Compose([transforms.Resize((224, 224)),
+                                  transforms.RandomHorizontalFlip(),
+                                  transforms.ToTensor()])
+#transformation to test imagese
+test_transforms=transforms.Compose([transforms.Resize((224, 224)),
+                                   transforms.RandomHorizontalFlip(),
+                                   transforms.ToTensor()])   
+
+class dataset(Dataset):
+  def __init__(self,file_list,transform=None):
+    self.file_list=file_list
+    self.transform=transform
+  def __len__(self):
+    self.filelength=len(self.file_list)
+    return self.filelength
+  def __getitem__(self,idx):
+    img_path=self.file_list[idx]
+    img=Image.open(img_path)
+    img_transformed=self.transform(img)
+    #get the lable 
+    lable=img_path.split('\\')[2]
+    #one-hot encoding the lable
+    if lable=='MildDemented':
+        lable=0
+    elif lable=='ModerateDemented':
+        lable=1
+    elif lable=='NonDemented':
+        lable=2
+    elif lable=='VeryMildDemented':
+        lable=3
+    return img_transformed,lable
+
+#loading the train,test and val lists
+train_list=np.load('train_list.npy')
+test_list=np.load('test_list.npy')
+val_list=np.load('val_list.npy')
+
+#creating the data with lables
+train_data=dataset(train_list,transform=train_transforms)
+val_data=dataset(val_list,transform=val_transforms)
+test_data=dataset(test_list,transform=test_transforms)
+
+#creating data loader for train val and test
+train_loader=DataLoader(dataset = train_data,batch_size=batch_size,shuffle=True)
+val_loader=DataLoader(dataset = val_data,batch_size=batch_size,shuffle=True)
+test_loader=DataLoader(dataset = test_data,batch_size=batch_size,shuffle=True)
+
+
+"""
+Lables:
+    MildDemented->0
+    ModerateDemented->1
+    NonDemented->2
+    VeryMildDemented->3
+"""
